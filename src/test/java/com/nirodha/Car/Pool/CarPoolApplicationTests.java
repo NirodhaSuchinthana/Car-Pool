0483d8eea2f654e210738e5428812a94463557ed
Nirodha Suchinthana <suchithanaapn@gmail.com>
2020-07-03 19:07:24 +0530

    .gitignore updated,basic configurations for swagger2 and mongodb

---
+++ src/test/java/com/nirodha/Car/Pool/CarPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirodha.Car.Pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
